3972c026c461ea0f1f774a46201c72691f8a57bc
MadSpun4 <95620197+MadSpun4@users.noreply.github.com>
2023-12-02 21:03:26 +1000

    Добавил несколько свойств классу

--- Human/Human.cpp
+++ Human/Human.cpp
@@ -5,13 +5,33 @@ using namespace std;
 class Human
 {
 private:
-    
+    string Name;
+    unsigned short int Age;
+    bool Gender;
+
 public:
-    Human();
-    ~Human();
+    Human(string nameVal, unsigned short int ageVal, bool genderVal) {
+        Name = nameVal;
+        Age = ageVal;
+        Gender = genderVal;
+    };
+    ~Human(){
+
+    };
+
+    friend ostream& operator<<(ostream& os, const Human& hm) {
+        os << "Name: " << hm.Name << "\n" << "Age: " << hm.Age << "\n"
+        << "Gender: " << (hm.Gender ? "Male" : "Female") << endl;
+ 
+        return os;
+    }
 };
 
 int main() {
-    Human Borya;
-    Human Ilya;
+    Human Borya("Borya", 22, true);
+    Human Ilya("Ilya", 22, true);
+    cout << Borya << endl;
+    cout << Ilya << endl;
+    
+    return 0;
 }
